ceddabb6f5823f7bbff7f2dc5184836b5582e729
Michael Rosenfield <mer95@case.edu>
2015-09-12 04:39:42 -0400

    Fixed the button presses

--- app/src/classes/ClassController.js
+++ app/src/classes/ClassController.js
@@ -11,30 +11,30 @@
         var self = this;
 
         self.selected = null;
-        self.classes = [ ];
-        self.toggleList = toggleClassesList;
+        self.classes = [];
         self.selectClass = selectClass;
+        self.toggleList = toggleClassesList;
 
-
-        classService.loadAllClasses()
-            .then(function(classes) {
+        classService
+            .loadAllClasses()
+            .then(function (classes) {
                 self.classes = [].concat(classes);
                 self.selected = classes[0];
-            })
+            });
+
+
+        function selectClass(course) {
+            self.selected = angular.isNumber(course) ? $scope.classes[course] : course;
+            self.toggleList();
+        }
+
+        function toggleClassesList() {
+            var pending = $q.when(true);
+
+            pending.then(function () {
+                $mdSidenav('left').toggle();
+            });
+        }
     }
-
-    function selectClass(course) {
-        self.selected = angular.isNumber(course) ? $q.classes[course] : course;
-        self.toggleList();
-    }
-
-    function toggleClassesList() {
-        var pending = $q.when(true);
-
-        pending.then(function(){
-            $mdSidenav('left').toggle();
-        });
-    }
-
 
 })();
